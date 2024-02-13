4faeeb65dd7f0d69557e55b75a116c6ece5c76f4
amirmtolouie@gmail.com <amirmtolouie@gmail.com>
2024-02-13 20:29:37 +0300

    project setup with navigation

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Fragment } from "react";
+import AllPlaces from "./screens/AllPlaces";
+import PlaceDetails from "./screens/PlaceDetails";
+import AddPlace from "./screens/AddPlace";
+import IconButton from "./components/UI/IconButton";
+const stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Fragment>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <stack.Navigator>
+          <stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add"
+                  size={24}
+                  color={tintColor}
+                  onPress={() => navigation.navigate("AddPlace")}
+                />
+              ),
+            })}
+          />
+          <stack.Screen name="AddPlace" component={AddPlace} />
+          <stack.Screen name="PlaceDetails" component={PlaceDetails} />
+        </stack.Navigator>
+      </NavigationContainer>
+    </Fragment>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
